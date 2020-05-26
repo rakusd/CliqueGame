56ff34317f82dc68b2ff0e6a50bd1806f6eeeb49
Michał Urawski <michurawski@gmail.com>
2020-05-26 22:29:23 +0200

    React app created

---
+++ ui/src/components/root-component.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Header } from '../header/header-component';
+import { Routes } from '../routes/routes-component';
+import { Footer } from '../footer/footer-component';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { UserStateProvider } from '../../context/user-state-provider';
+import * as LocalStorageService from '../../services/local-storage-service';
+import Container from '@material-ui/core/Container';
+
+export function Root() {
+    const localStorageState = LocalStorageService.getObject('userState');
+    const initialState = localStorageState !== null ? localStorageState : {
+        isLogged: false,
+        name: '',
+    };
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+          case 'setUser':
+            const newObj = {
+              ...state,
+              isLogged: true,
+              name: action.name,
+              token: action.token
+            };
+            LocalStorageService.setObject('userState', newObj);
+            return newObj;
+          case 'deleteUser':
+            LocalStorageService.deleteObject('userState');
+            return {
+              name :'',
+              isLogged: false
+            }   
+          default:
+            return state;
+        }
+      };
+    return (
+        <UserStateProvider initialState={initialState} reducer={reducer}>
+            <BrowserRouter>
+                <CssBaseline />
+                <Header />
+                <Container>
+                  <Routes />
+                </Container>
+            </BrowserRouter>
+        </UserStateProvider>
+    );
+}
---
+++ ui/webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+require("@babel/polyfill");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+});
+
+module.exports = {
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)?$/,
+                exclude: /node_modules/,
+                include: [
+                    path.resolve(__dirname, 'src'),
+                    path.resolve(__dirname, 'index.js')
+                ],
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    }]
+            },
+            {
+                test: /\.(gif|svg|jpg|png)$/,
+                loader: "file-loader",
+            }
+        ],
+    },
+    devServer: {
+        historyApiFallback: true,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "*"
+        }
+    },
+    plugins: [htmlPlugin],
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    node: { fs: 'empty' }
+};
