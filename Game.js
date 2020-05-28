76296d4b0b51159d1fb40e58fd81cad171360faa
Damian <damian.rakus@gmail.com>
2020-05-28 22:26:31 +0200

    Changes for UI

--- Game.js
+++ Game.js
@@ -24,6 +24,8 @@ class Game {
         this.player1 = this.initPlayer(config, config.player1, PLAYER1);
         this.player2 = this.initPlayer(config, config.player2, PLAYER2);
         this.humanMove = false;
+        this.whichBotMoves = PLAYER1;
+        this.lastMove = null;
     }
 
     initPlayer(gameConfig, playerConfig, playerId) {
@@ -49,6 +51,7 @@ class Game {
         return player;
     }
 
+    // intializes bot vs player game
     startHumanComputerGame() {
         let move = null;
         if (this.player1 !== null) {
@@ -63,19 +66,23 @@ class Game {
             this.botPlayer = this.player2;
         }
         this.humanMove = true;
+        this.lastMove = move;
         return move;
     }
-
+    
+    // use this in bot vs human
     makeOnlyHumanMove(move) {
         if (!this.humanMove) {
             throw "Bot's turn!";
         }
+
         this.humanMove = false;
+        
         try {
             this.makeMove(move, this.humanId);
             if (checkIfPlayerWon(this.humanId)) {
                 this.winner = this.humanId;
-                return 0; //human win
+                return this.humanId; //human win
             }
         } catch (error) {
             console.error(error);
@@ -84,31 +91,113 @@ class Game {
         }
 
         if (!this.canMove()) {
-            return 2; // no moves
+            return 0; // no moves = draw
         }
 
-        return 1; // still game
+        this.lastMove = move;
+        return 3; // still game, now call makeOnlyBotMove()
     }
 
+    // use this in bot vs human
     makeOnlyBotMove() {
         if (this.humanMove) {
             throw "Human's turn"
         }
-        let botMove = this.botPlayer.decideMove(move, this.board.copyBoard());
+        let botMove = this.botPlayer.decideMove(this.lastMove, this.board.copyBoard());
 
-        this.board.markMove(move, this.botMove);
-        if (checkIfPlayerWon(this.botMove)) {
+        this.board.markMove(botMove, this.botId);
+        if (checkIfPlayerWon(this.botId)) {
             this.winner = this.botId;
-            return 3; //bot win
+            return this.botId; //bot win
         }
 
         if (!this.canMove()) {
-            return 2; // no moves
+            return 0; // no moves = draw
         }
 
+        this.humanMove = true;
+        this.lastMove = move;
         return move; // still game, need to return move for UI
     }
 
+    // use for one move in bot vs bot game
+    makeMoveInBotVsBot() {
+        let currentBot = null;
+
+        if(this.whichBotMoves === PLAYER1) {
+            currentBot = this.player1;
+        } else {
+            currentBot = this.player2;
+        }
+
+        let botMove = currentBot.decideMove(this.lastMove, this.board.copyBoard());
+
+        this.board.markMove(botMove, this.whichBotMoves);
+        if (checkIfPlayerWon(this.whichBotMoves)) {
+            this.winner = this.whichBotMoves;
+            return this.whichBotMoves; //bot win
+        }
+
+        if (!this.canMove()) {
+            return 0; // no moves = draw
+        }
+
+        this.whichBotMoves = this.whichBotMoves === PLAYER1 ? PLAYER2 : PLAYER1;
+        this.lastMove = move;
+        
+        return move; // still game, need to return move for UI
+    }
+
+    // used to play whole game, returns winner
+    playAutomaticGameOfBots() {
+        let lastMove = null;
+
+        while (true) {
+            if (!this.canMove()) {
+                break;
+            }
+            lastMove = this.player1.decideMove(lastMove, this.board.copyBoard());
+            this.makeMove(lastMove, PLAYER1);
+            if (this.checkIfPlayerWon(PLAYER1)) {
+                this.winner = PLAYER1;
+                return this.winner;
+            }
+
+            if (!this.canMove()) {
+                break;
+            }
+            lastMove = this.player2.decideMove(lastMove, this.board.copyBoard());
+            this.makeMove(lastMove, PLAYER2);
+            if (this.checkIfPlayerWon(PLAYER2)) {
+                this.winner = PLAYER2;
+                return this.winner;
+            }
+        }
+
+        // no winner = draw
+        return 0;
+    }
+
+    makeMove(move, player) {
+        process.stdout.write(player === PLAYER1 ? '-' : '+');
+        this.board.markMove(move, player);
+        this.movesHistory.push(move);
+        this.moveCount++;
+    }
+
+    canMove() {
+        return this.maxMoves > this.moveCount;
+    }
+
+    checkIfPlayerWon(player) {
+        if (this.minMoves > (this.moveCount + 1) / 2) {
+            return false;
+        }
+
+        return this.board.doesCliqueExist(this.cliqueSize, player);
+    }
+
+    //=====================================DEPRECATED
     makeHumanPlayerMove(move) {
         if (!this.humanMove) {
             throw 'Bot is thinking!';
@@ -135,54 +224,6 @@ class Game {
         }
 
         return botMove;
-    }
-
-    playAutomaticGameOfBots() {
-        let lastMove = null;
-
-        while (true) {
-            if (!this.canMove()) {
-                break;
-            }
-            lastMove = this.player1.decideMove(lastMove, this.board.copyBoard());
-            this.makeMove(lastMove, PLAYER1);
-            if (this.checkIfPlayerWon(PLAYER1)) {
-                this.winner = PLAYER1;
-                return this.winner;
-            }
-
-            if (!this.canMove()) {
-                break;
-            }
-            lastMove = this.player2.decideMove(lastMove, this.board.copyBoard());
-            this.makeMove(lastMove, PLAYER2);
-            if (this.checkIfPlayerWon(PLAYER2)) {
-                this.winner = PLAYER2;
-                return this.winner;
-            }
-        }
-
-        // no winner
-        return null;
-    }
-
-    makeMove(move, player) {
-        process.stdout.write(player === PLAYER1 ? '-' : '+');
-        this.board.markMove(move, player);
-        this.movesHistory.push(move);
-        this.moveCount++;
-    }
-
-    canMove() {
-        return this.maxMoves > this.moveCount;
-    }
-
-    checkIfPlayerWon(player) {
-        if (this.minMoves > (this.moveCount + 1) / 2) {
-            return false;
-        }
-
-        return this.board.doesCliqueExist(this.cliqueSize, player);
     }
 }
 
